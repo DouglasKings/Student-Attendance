99f92e7e8bd9d8176d2360b262bbb8ea6bfffe28
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-05-31 00:48:21 +0300

    Student Attendance

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Student Attendance
- This is a C program that checks if a student is allowed to sit in exam or not
+ A student will not be allowed to sit in exam if his/her attendence is less than 75%. Write a C program to Take following input from user;
+   Number of classes held
+   Number of classes attended.
+   And print
+   Percentage of class attended
+   Is student is allowed to sit in exam or not.
---
+++ StudentAttendance.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void studentAttendance(double Attendance); 
+
+int main(){
+    double classesHeld, classesAttended, Attendance;
+
+    printf("Enter the number of classes held: ");
+    scanf("%lf", &classesHeld);
+    printf("Enter the number of classes attended: ");
+    scanf("%lf", &classesAttended);
+
+    Attendance = (classesAttended / classesHeld) * 100; 
+    studentAttendance(Attendance); 
+
+    printf("The number of classes held: %.2lf \n", classesHeld);
+    printf("The number of classes attended: %.2lf \n", classesAttended);
+    printf("The percentage of class attended: %.2lf%% \n", Attendance); 
+
+    return 0;
+}
+
+void studentAttendance(double Attendance){ 
+    if(Attendance < 75.0){ 
+        printf("The student is not allowed to sit in an exam. \n");
+    } else {
+        printf("The student is allowed to sit in an exam. \n");
+    }
+}
